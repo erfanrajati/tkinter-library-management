c7ee34a17f6fb52cdba6a70c9781c96709f18b30
Erfan Rajati Haghi <109225834+erfanrajati@users.noreply.github.com>
2024-08-08 15:34:38 +0330

    Back end update

--- backend.py
+++ backend.py
@@ -4,15 +4,36 @@ class Book:
     def __init__(self, id, name, year, author):
         self.id = id
         self.name = name
-        self.author = author
         self.year = year
+        self.author = author
+
+    def get_key_value(self):
+        key_value = {
+            "name":self.name,
+            "year":self.year,
+            "author":self.author
+        }
+        return key_value
 
 class Library:
     def __init__(self):
-        self.database = json.load(open('./database.json'))
-    
-    def add_book(self):
-        pass
+        file = open("./database.json", "r")
+        self.database: dict = json.load(file)
+
+
+    def add_book(self, id, name, year, author):
+        if id in self.database.keys():
+            raise ValueError("id already exists")
+        else:
+            newBook = Book(id, name, year, author)
+            self.database[id] = newBook.get_key_value()
+            file = open("./database.json", "w")
+            json.dump(
+                self.database, 
+                file,
+                indent=4
+            )
+
 
     def remove_book(self):
         pass
@@ -25,11 +46,36 @@ class Library:
 
     def get_book_names(self):
         result = [
-            self.database[book]["name"] 
-            for book in self.database.keys()
+            self.database[id]["name"] 
+            for id in self.database.keys()
         ]
         return result
 
 
 library = Library()
-print(library.get_book_names())
+library.add_book(
+    "id0004", 
+    "Harry Potter 4",
+    2009,
+    "J.K. Rolling"
+)
+library.add_book(
+    "id0005", 
+    "Harry Potter 4",
+    2009,
+    "J.K. Rolling"
+)
+library.add_book(
+    "id0006", 
+    "Harry Potter 4",
+    2009,
+    "J.K. Rolling"
+)
+library.add_book(
+    "id0001", 
+    "Harry Potter 4",
+    2009,
+    "J.K. Rolling"
+)
+
+
