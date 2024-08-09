0e659e838519913b35d8cfa8270464622f2c383d
Erfan Rajati Haghi <erfan.rajati@gmail.com>
2024-08-09 13:02:44 +0330

    feat/ auto id generator added

---
+++ Test/test.py
@@ -0,0 +1,9 @@
+import sys
+sys.path.insert(0, "D:/Programming/Open Source/tkinter-library-management/")
+from backend import *
+
+library = Library()
+
+# library.add_book("Whimpy Kid", 2011, "Jeff Kinny")
+# newBook = Book()
+# newBook.generate_id()
--- backend.py
+++ backend.py
@@ -1,11 +1,18 @@
 import json
 
 class Book:
-    def __init__(self, id, name, year, author):
-        self.id = id
+    def __init__(self, name, year, author):
+        self.id = self.generate_id()
         self.name = name
         self.year = year
         self.author = author
+
+    # initializer for testing
+    # def __init__(self):
+    #     # self.id = self.generate_id()
+    #     self.name = "name"
+    #     self.year = "year"
+    #     self.author = "author"
 
     def get_key_value(self):
         key_value = {
@@ -15,25 +22,38 @@ class Book:
         }
         return key_value
 
+    def generate_id(self):
+        file = open("./database.json", "r")
+        db: dict = json.load(file)
+        id_list = [int(id.split('-')[1]) for id in db.keys()]
+        id_list.sort()
+        for i in range(id_list[0], id_list[-1]+2):
+            if i not in id_list:
+                new_id = i
+                break
+        return "id-" + str(10000 + new_id)[1:]
+        # print("id-" + str(10000 + new_id)[1:])
+
 class Library:
     def __init__(self):
         file = open("./database.json", "r")
         self.database: dict = json.load(file)
 
+    def get_book_names(self):
+        result = [
+            self.database[id]["name"] 
+            for id in self.database.keys()
+        ]
+        return result
 
-    def add_book(self, id, name, year, author):
-        if id in self.database.keys():
-            raise ValueError("id already exists")
-        else:
-            newBook = Book(id, name, year, author)
-            self.database[id] = newBook.get_key_value()
-            file = open("./database.json", "w")
-            json.dump(
-                self.database, 
-                file,
-                indent=4
-            )
-
+    def add_book(self, name, year, author):
+        # if id in self.database.keys():
+        #     raise ValueError("id already exists")
+        # else:
+        newBook = Book(name, year, author)
+        self.database[newBook.id] = newBook.get_key_value()
+        file = open("./database.json", "w")
+        json.dump(self.database, file, indent=4)
 
     def remove_book(self):
         pass
@@ -44,38 +64,7 @@ class Library:
     def get_all_books(self):
         pass
 
-    def get_book_names(self):
-        result = [
-            self.database[id]["name"] 
-            for id in self.database.keys()
-        ]
-        return result
 
 
-library = Library()
-library.add_book(
-    "id0004", 
-    "Harry Potter 4",
-    2009,
-    "J.K. Rolling"
-)
-library.add_book(
-    "id0005", 
-    "Harry Potter 4",
-    2009,
-    "J.K. Rolling"
-)
-library.add_book(
-    "id0006", 
-    "Harry Potter 4",
-    2009,
-    "J.K. Rolling"
-)
-library.add_book(
-    "id0001", 
-    "Harry Potter 4",
-    2009,
-    "J.K. Rolling"
-)
 
 
